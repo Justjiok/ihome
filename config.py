72510606b87704c8e1f59c1e908dd9c425241f2a
Justjiok <1318572615@qq.com>
2019-01-18 15:45:50 +0800

    配置日志， 代码抽取优化

---
+++ config.py
@@ -0,0 +1,37 @@
+import logging
+import redis
+
+
+class Config(object):
+    DEBUG = True
+    # 数据库配置信息
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #redis配置
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+
+    """工程配置信息"""
+    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
+    # flask_session配置信息
+    SESSION_TYPE = 'redis'
+    SESSION_USE_SIGNER = True  # 让cookie中的session——id被加密签名处理
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    PERMANENT_SESSION_LIFETIME = 86400  # 设置session的有效期
+
+
+class DevelopementConfig(Config):
+    DEBUG = True
+    LOG_LEVEL = logging.DEBUG
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    LOG_LEVEL = logging.ERROR
+
+
+# 定义配置字典
+config = {
+    "development": DevelopementConfig,
+    "production": ProductionConfig
+}
--- manage.py
+++ manage.py
@@ -1,40 +1,10 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import redis
-from flask_wtf.csrf import CSRFProtect
-from flask_session import Session
+
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from info import create_app, db
 
 
-app = Flask(__name__)
-
-class Config(object):
-    DEBUG = True
-    # 数据库配置信息
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/information'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    #redis配置
-    REDIS_HOST = '127.0.0.1'
-    REDIS_PORT = 6379
-
-    """工程配置信息"""
-    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
-    # flask_session配置信息
-    SESSION_TYPE = 'redis'
-    SESSION_USE_SIGNER = True  # 让cookie中的session——id被加密签名处理
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    PERMANENT_SESSION_LIFETIME = 86400  # 设置session的有效期
-
-
-
-
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-Session(app)
-# csrf防护
-CSRFProtect(app)
+app = create_app('development')
 # 数据库迁移扩展
 manager = Manager(app)
 Migrate(app, db)
